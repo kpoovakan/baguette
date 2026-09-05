f9e4b1d7b2180c269227c8e5698ac949bf92682b
kpoovakan <217997837+kpoovakan@users.noreply.github.com>
2026-09-05 16:26:56 -0700

    Add button to test save URL

--- indexfiles/script.js
+++ indexfiles/script.js
@@ -14,6 +14,18 @@ function userLogin() {
         `width=${width},height=${height},top=${top},left=${left}`
     );
 }
+function userSave() {
+    const width = 600;
+    const height = 700;
+    const left = window.screenX + (window.outerWidth - width) / 2;
+    const top = window.screenY + (window.outerHeight - height) / 2;
+
+    window.open(
+        `${worker}/save`,
+        'login to Baguette with GitHub OAuth',
+        `width=${width},height=${height},top=${top},left=${left}`
+    );
+}
 const worker = "https://baguette.kpoovakan.workers.dev";
 window.addEventListener("message", (event) => {
     if (event.origin !== worker) return;
