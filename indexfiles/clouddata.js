e8ff18cbef1451908618842d9d6071a2cff806ab
kpoovakan <217997837+kpoovakan@users.noreply.github.com>
2026-09-05 18:59:13 -0700

    Create color theme files and organize clouddata scripts

---
+++ indexfiles/clouddata.js
@@ -0,0 +1,41 @@
+"use strict";
+
+window.addEventListener("message", (event) => {
+    if (event.origin !== worker) return;
+    const data = event.data;
+    if (data && data.type === 'GITHUB_AUTH_SUCCESS') {
+        console.log("Received data from GitHub OAuth: ", data.payload);
+        const username = data.payload.username;
+        document.getElementById("userInfo").innerText = username;
+    } else if (data && data.type === 'GITHUB_SAVE_SUCCESS') {
+        console.log("Save successful!");
+    }
+});
+
+function userLogin() {
+    const width = 600;
+    const height = 700;
+    const left = window.screenX + (window.outerWidth - width) / 2;
+    const top = window.screenY + (window.outerHeight - height) / 2;
+
+    window.open(
+        `${worker}/login`,
+        'login to Baguette with GitHub OAuth',
+        `width=${width},height=${height},top=${top},left=${left}`
+    );
+}
+
+function userSave() {
+    const width = 600;
+    const height = 700;
+    const left = window.screenX + (window.outerWidth - width) / 2;
+    const top = window.screenY + (window.outerHeight - height) / 2;
+
+    window.open(
+        `${worker}/save`,
+        'login to Baguette with GitHub OAuth',
+        `width=${width},height=${height},top=${top},left=${left}`
+    );
+}
+
+const worker = "https://baguette.kpoovakan.workers.dev";
---
+++ indexfiles/color.js
@@ -0,0 +1,102 @@
+"use strict";
+// to ensure correct css styles, connect this javascript file to every html document
+// if javascript isn't supported, the default "madamoiselle" theme is used
+
+/* url has dark mode first, with darkest to lightest
+
+// sunshine and solarEclipse
+// https://coolors.co/382d00-b19206-fcefb4-f6cd13-fae588-fdf8e1
+// array has light mode first, with lightest to darkest
+const sunshine = `[
+    "fdf8e1",
+    "fae588",
+    "f6cd13"
+]`;
+const solarEclipse = `[
+    "fcefb4",
+    "b19206",
+    "382d00"
+]`;
+
+// durian
+// https://coolors.co/000966-ff33dd-f8ff33
+// dark mode, with lightest to darkest
+const durian = `[
+    "f8ff33",
+    "ff33dd",
+    "000966"
+]`;
+
+// deprecated
+// https://coolors.co/182d3b-68a1b3-7cd5df-d6daff
+// array has light mode first, with lightest to darkest
+const pastelAll = `{
+    "light": [
+        "d6daff",
+        "7cd5df",
+        "182d3b"
+    ],
+    "dark": [
+        "d6daff",
+        "68a1b3",
+        "182d3b"
+    ]
+}`;
+
+// pastel
+// https://coolors.co/182d3b-5d6f9e-d6daff
+// light mode, with lightest to darkest
+const pastel = `[
+    "d6daff",
+    "5d6f9e",
+    "182d3b"
+]`;
+
+// tofu
+// https://coolors.co/3b362b-a0b69e-d3dcd0
+// dark mode, with lightest to darkest
+const tofu = `[
+    "d3dcd0",
+    "a0b69e",
+    "3b362b"
+]`;
+
+// madamoiselle
+// https://coolors.co/123538-c8d4c4-f7deed
+// dark mode, with lightest to darkest
+const madamoiselle = `[
+    "f7deed",
+    "c8d4c4",
+    "123538"
+]`;
+*/
+
+
+const colorThemes = `{
+    "sunshine": ["#fffcf0","#fae588","#c4a20e"],
+    "solarEclipse": ["#382d00","#b19206","#fcefb4"],
+    "durian": ["#4D0054","#ff33dd","#f8ff33"],
+    "pastel": ["#d6daff","#5d6f9e","#182d3b"],
+    "tofu": ["#3b362b","#a0b69e","#d3dcd0"],
+    "madamoiselle": ["#123538","#c8d4c4","#f7deed"]
+}`;
+
+
+window.addEventListener("load", function() {
+    const theme = window.localStorage.getItem("preferencesTheme");
+    const colors = JSON.parse(colorThemes);
+
+    if (theme == null || theme == undefined || theme == "") {
+        return;
+    }
+
+    const array = colors[theme];
+    const background = array[0];
+    const accent = array[1];
+    const foreground = array[2];
+    const stylish = document.documentElement.style;
+
+    stylish.setProperty("--colorBackground", background);
+    stylish.setProperty("--colorForeground", foreground);
+    stylish.setProperty("--colorAccent", accent);
+});
