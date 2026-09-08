9f4a6911475c295938371cd83b9a2bb33246ad26
kpoovakan <217997837+kpoovakan@users.noreply.github.com>
2026-09-07 17:49:44 -0700

    Change outcome of "view plans" button

--- indexfiles/script.js
+++ indexfiles/script.js
@@ -2,11 +2,6 @@ window.addEventListener("load", function() {
     console.log("load");
 });
 
-function mainViewPlans() {
-    const viewPlans = `/pricing`;
-    window.location.href = viewPlans;
-}
-
 function clouddataLogin() {
     userLogin();
 }
